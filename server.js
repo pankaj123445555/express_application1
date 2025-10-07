2eb28eccd92ab9b7cbb08d7b832c00e3ebf610cb
pankaj_421 <pankaj.giri@sukshi.com>
2025-10-07 10:36:07 +0530

    adding an docker file

--- server.js
+++ server.js
@@ -13,11 +13,15 @@ require("./config/db");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.get("/user", (req, res) => {
+  return res.send("hi pankaj");
+});
 
 app.use(rateLimiter);
 
